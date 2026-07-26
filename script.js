9fa57ca5f7ebb91c1fc4b0c48ebc1c27d46a367b
btinvitation <ngkj5zgdh4@privaterelay.appleid.com>
2026-07-26 06:05:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,105 @@
+// ====================================
+// Bhuvaneshwari ❤️ Thyagu
+// Premium Invitation
+// ====================================
 
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        document.getElementById("loader").style.opacity = "0";
+
+        setTimeout(() => {
+
+            document.getElementById("loader").style.display = "none";
+
+            document.getElementById("website").style.display = "block";
+
+            document.getElementById("website").style.opacity = "0";
+
+            setTimeout(() => {
+
+                document.getElementById("website").style.transition = "1.2s";
+
+                document.getElementById("website").style.opacity = "1";
+
+            },100);
+
+        },1000);
+
+    },2500);
+
+});
+
+
+// ==============================
+// Envelope
+// ==============================
+
+const envelope=document.querySelector(".envelope");
+
+const book=document.getElementById("book-section");
+
+book.style.display="none";
+
+envelope.addEventListener("click",()=>{
+
+    envelope.style.transform="scale(.92) rotateX(18deg)";
+
+    envelope.style.transition=".7s";
+
+    setTimeout(()=>{
+
+        envelope.parentElement.style.display="none";
+
+        book.style.display="flex";
+
+        book.scrollIntoView({
+            behavior:"smooth"
+        });
+
+    },700);
+
+});
+
+
+// ==============================
+// Smooth Fade
+// ==============================
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.animate([
+
+{
+opacity:0,
+transform:"translateY(80px)"
+},
+
+{
+opacity:1,
+transform:"translateY(0)"
+}
+
+],{
+
+duration:1200,
+fill:"forwards"
+
+});
+
+}
+
+});
+
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+observer.observe(section);
+
+});
